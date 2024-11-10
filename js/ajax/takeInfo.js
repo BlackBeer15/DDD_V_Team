bb7bce0e64667151197752092d3d17af996dc25f
BlackBeer15 <little4biz@gmail.com>
2024-11-10 12:49:18 +0300

    eleventh commit

--- js/ajax/takeInfo.js
+++ js/ajax/takeInfo.js
@@ -6,7 +6,7 @@ $('input[name="send-photo"]').click(function (e) {
 	sendData.append('photoTower', $('input[name="photoTower"')[0].files[0]);
 
     $.ajax({
-		url: 'http://192.168.101.33:8000/getByImage',
+		url: 'http://192.168.137.1:8000/getByImage',
 		type: 'POST',
 		cache: false,
 		contentType: false,
@@ -17,19 +17,12 @@ $('input[name="send-photo"]').click(function (e) {
 		success (data) {
 			$('.photoForm').css({'display':'none'});
 			$('.addressForm').css({'display':'none'});
+			$('.container-button').css({'display':'none'});
 			$('.response-container').css({'display':'flex'});
 			$('.tryAgain').css({'display':'flex'});
             $('.response-container').append(
-				'<div class="for-image">',
-                	'<img src="images/example.jpg" alt="photo">',
-            	'</div>',
-				'<div class="for-text">',
-					'<p>Наименование: <span></span></p>',
-					'<p>Адрес: <span></span></p>',
-					'<p>Год постройки: <span></span></p>',
-					'<p>Краткая историческая справка: <span><span></p>',
-            	'</div>',
-			);
+				'<div class="for-image"><img src="images/example.jpg" alt="photo"></div><div class="for-text"><p>Наименование: <span>'+data.place+'</span></p><p>Адрес: <span>'+data.address+'</span></p><p>Год постройки: <span>'+data.year+'</span></p><p>Краткая историческая справка: <span>'+data.history+'<span></p></div>'
+				);
         }
     })
 })
